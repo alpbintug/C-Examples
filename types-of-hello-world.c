e841425007cb458cd2aee1298028ef5fa4381672
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-11 15:25:41 +0300

    Some changes

--- types-of-hello-world.c
+++ types-of-hello-world.c
@@ -5,12 +5,14 @@ int main(){
     //Printing "Hello world with different methods
 
     printf("Hello World 1\n");
-    puts("Hello World 2\n");
+    puts("Hello World 2\n");//Puts the string "Hello World 2" to the console
 
     //Using char* to print hello world;
     char *helloWorld="Hello World 3\n";
-    while(putchar(*helloWorld++));
+    //Putting each character until string ends
+    while(putchar(*helloWorld++));//putchar function returns a integer value, if it returns a 0, that means the next char is \0
     helloWorld = "Hello World 4\n";
+    //First value 1 means writing mode, middle valua is the string we want to write, last value is the lenght
     write(1,helloWorld,14);
     helloWorld = "Hello World 5\n";
     printf("%s",helloWorld);
