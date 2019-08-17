bce6f0c50cd4a83815fed2ed24be747670a20141
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-17 21:19:37 +0300

    Update 6-Arrays.c

--- 6-Arrays.c
+++ 6-Arrays.c
@@ -3,7 +3,9 @@
 
 int main(){
     int i;
-    int array1[5];
+    int array1[5];//In C, you have to give an array a size when defining it.
+    //If you create an array like this, compiler will allocate 6 slots of memory for this array, first five will be reserved for your data,
+    //and the last one will hold the information about end of the array (it is generally null)               
     array1[0]=1;
     array1[1]=2;
     array1[2]=3;
@@ -12,10 +14,12 @@ int main(){
     for(i=0;i<5;i++){
         printf("%d ",array1[i]);
     }
+    //Output: 1 2 3 4 5
     printf("\n");
-    int array2[] = {1,2,3,4,5};
+    int array2[] = {1,2,3,4,5}; //You can assign values to your array when defining it like this.
     for(i=0;i<5;i++){
         printf("%d ",array2[i]);
     }
+    //Output: 1 2 3 4 5
 
 }
