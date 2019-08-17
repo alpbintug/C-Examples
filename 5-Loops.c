d14fd0b615fd6ccca70ccb1c05ac741c02563e17
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-17 18:03:53 +0300

    Update 5-Loops.c

--- 5-Loops.c
+++ 5-Loops.c
@@ -8,10 +8,11 @@ int main(){
         printf("%d",i);
     }
     i=0;
-    while(1){
+    //while(1) loop equals while(true) loop in high level languages, which is an infinite loop, be careful while using this.
+    while(1){//Unlike in high level languages, C checks conditions with 0 and 1, not true and false (They are the basically same thing, 1 in Logic means true, 0 means false)
         i++;
         if(i>=10)
-            break;
+            break;//I do NOT recommend using "break" command. It uses too high resource for a low level language like C. Make your conditions clearer instead of using "break"
         printf("%d",i);
     }
     i=0;
@@ -19,6 +20,8 @@ int main(){
         print("%d",i);
         i++;
     }while(i<10);
+    //Difference between do...while loop and while loop is similar with i++ and ++i, i++ uses the i value first, then increases it, ++i increases the i value and uses it later.
+    //do...while loop uses the code inside the loop first, then checks for condition, while loop checks for condition first, then uses the code.
     for(i=0;i<10;i++){
         printf("%d",i);
     }
