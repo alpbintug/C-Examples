7e06e019dc946536c312c01690dcc0413c92fdc7
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-17 18:07:36 +0300

    Update 5-Loops.c

--- 5-Loops.c
+++ 5-Loops.c
@@ -7,6 +7,7 @@ int main(){
         i++;
         printf("%d ",i);
     }
+    //Output: 1 2 3 4 5 6 7 8 9 10
     i=0;
     printf("\n");
     //while(1) loop equals while(true) loop in high level languages, which is an infinite loop, be careful while using this.
@@ -16,16 +17,28 @@ int main(){
             break;//I do NOT recommend using "break" command. It uses too high resource for a low level language like C. Make your conditions clearer instead of using "break"
         printf("%d ",i);
     }
+    //Output: 1 2 3 4 5 6 7 8 9
     printf("\n");
     i=0;
     do{
         printf("%d ",i);
         i++;
     }while(i<10);
+    //Output: 0 1 2 3 4 5 6 7 8 9
     printf("\n");
     //Difference between do...while loop and while loop is similar with i++ and ++i, i++ uses the i value first, then increases it, ++i increases the i value and uses it later.
     //do...while loop uses the code inside the loop first, then checks for condition, while loop checks for condition first, then uses the code.
     for(i=0;i<10;i++){
         printf("%d ",i);
     }
+    //Output: 0 1 2 3 4 5 6 7 8 9
+    
+    
+    /*
+    Output of the entire code:
+    1 2 3 4 5 6 7 8 9 10
+    1 2 3 4 5 6 7 8 9
+    0 1 2 3 4 5 6 7 8 9
+    0 1 2 3 4 5 6 7 8 9*/
 }
+
