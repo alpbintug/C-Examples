0bcfe17e2f45950919f34274cbef82375bfa5eda
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-17 21:29:20 +0300

    Update 6-Arrays.c

--- 6-Arrays.c
+++ 6-Arrays.c
@@ -21,5 +21,35 @@ int main(){
         printf("%d ",array2[i]);
     }
     //Output: 1 2 3 4 5
+    printf("\n");
+    char string1[5];
+    string1[0]='H';
+    string1[1]='e';
+    string1[2]='l';
+    string1[3]='l';
+    string1[4]='o';
+    for(i=0;i<5;i++){
+        printf("%c",string1[i]);
+    }
+    //Output: Hello
+    printf("\n");
+    char string2[5] = "Hello";
+    string2[5]=0;//If you want to write your char array out as a string using %s, you have to set your last char to null, otherwise you will see (with a high chance) meaningless characters.
+    printf("%s\n",string2);
+    //Output: Hello
+    char string3[5] = {'H','e','l','l','o'};//You can define char arrays just like integer arrays
+    string3[5]=0;
+    printf("%s\n",string3);
+    //Output: Hello
+    char string4[5] = "Hel";//You dont need to put the entire data when defining the char array.
+    string4[3]='l';//You can manipulate your array afterwards.
+    string4[4]='o';
+    for(i=0;i<5;i++){
+        printf("%c",string4);
+    }
+    //Output: Hello
+    string4[5]=0;//Setting the last character to null to print the char array out as string
+    printf("\n%s\n",string4);
+    //Output: Hello
 
 }
