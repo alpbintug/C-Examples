1e93dc317272b09a92a2b60b94b6d7c2627f45d7
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-18 12:54:19 +0300

    Create E2-2048.c

---
+++ E2-2048.c
@@ -0,0 +1,4 @@
+/*Not %100 working
+I will try to fix the issue in upcoming weeks.
+Basically the issue is; if you Press 'd' for example, the values starting from 0 goes right, but they can't go in the last column if they're not in the n-1th column.
+*/
