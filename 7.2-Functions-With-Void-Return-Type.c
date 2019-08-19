6e2617c003e2b38b1f771429e7cda295d958f92a
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-19 21:36:12 +0300

    Update and rename 7.2-Functions-With-Void-Return-Type to 7.2-Functions-With-Void-Return-Type.c

--- 7.2-Functions-With-Void-Return-Type.c
+++ 7.2-Functions-With-Void-Return-Type.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /*Functions are generally used to simplify the code structure and they make the code easier to read.
 One of the main goals of the functions is making repeatative code blocks easier to call.*/
+
+//Void return type means, the functions is not returning any data type, so it cannot be assigned unlike pow() function.
+//You don't need to assign every function with a return type though. For example, printf returns the number of characters printed in integer format, but most of the time you don't assign printf to any integer.
+//Remember! Functions always need to return. Functions are basically "Jump" commands in code flow, you have to "Jump back" to the point you left, to do so, every function, uses "return"
 void printOperationResult(float x, float y,  int operationOffset){
     if(operationOffset==0){
         printf("%f",x+y);
