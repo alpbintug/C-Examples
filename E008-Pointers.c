95020ad4b3f3c1ba59e29b2d3dab8dc69c414ca5
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-23 23:01:11 +0300

    Update E008-Pointers.c

--- E008-Pointers.c
+++ E008-Pointers.c
@@ -4,7 +4,7 @@
 int main(){
     int *intP;
     char *charP1;
-    char *charP2 = "This is how you declare a string";
+    char *charP2 = "This is how you declare a string";//If you want to create a string using this method, you have to assign the string while defining the char*
     double *doubleP;
     float *floatP;
 
@@ -14,10 +14,13 @@ int main(){
     float varFloat = 567.23463;
 
     intP = &varInt;
+    //We assign the adress of given value to the given pointer, remember '&' symbol is also used in scanf and it helps you to read the value to the address of given variable
     printf("Value of integer is: %d\nAddress of integer: %x\nValue held by intPointer is: %d\nAddress held by intPointer is: %x\n",varInt,&varInt,*intP,intP);
     charP1 = &varChar;
+    //All of the addresses is hold in integer values, so you will assign an integer value to a char pointer, this could sound weird, but main job of pointers is to hold the location of the given variable.
     printf("\nValue of character is: %c\nAddress of character:  %x\nValue held by charPointer is: %c\nAddress held by charPointer is: %x\n",varChar,&varChar,*charP1,charP1);
     printf("\nString held by charPointer2:%s\nAddress held in charPointer2: %x\n",charP2,charP2);
+    //As we saw in previous examples, %s command reads chars until it sees a null character. Remember; char pointer hold the starting address of the given string, so you won't need use '*' symbol to print out the string.
     doubleP = &varDouble;
     printf("\nValue of double is: %lf\nAddress of double: %x\nValue held by doublePointer is: %lf\nAddress held by doublePointer is: %x\n",varDouble,&varDouble,*doubleP,doubleP);
     floatP = &varFloat;
