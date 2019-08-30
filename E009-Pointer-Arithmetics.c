7799d505df15eb707faa51224813bf03e49d86ff
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-30 22:35:22 +0300

    Update E009-Pointer-Arithmetics.c

--- E009-Pointer-Arithmetics.c
+++ E009-Pointer-Arithmetics.c
@@ -11,8 +11,7 @@ int main(){
     printf("Third char of the string: %c\n",*ptr);
     ptr-=2;
     printf("First char of the string: %c\n",*ptr);
-    /*
-    Output:
+    /*Output:
 
     First char of the string: T
     Second char of the string: h
@@ -20,5 +19,22 @@ int main(){
     First char of the string: T
     */
 
+    int arrInt[4] = {0xAA,0x01,0x10,0x14};
+    int *ptrInt = arrInt;
+    printf("First value of array: %x\n",*ptrInt);
+    ptrInt++;
+    printf("First increment of pointer: %x\n",*ptrInt);
+    ptrInt++;
+    printf("Second increment of pointer: %x\n",*ptrInt);
+    ptrInt++;
+    printf("Third increment of pointer: %x\n",*ptrInt);
+
+    /*Output:
+    First value of array: aa
+    First increment of pointer: 1
+    Second increment of pointer: 10
+    Third increment of pointer: 14
+    */
+    
     return 0;
 }
